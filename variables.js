801e49b6ffcdff8bfdebfd251151266fc984cce6
Dignory Carmona <dignory.luna777@gmail.com>
2025-04-16 22:56:51 -0500

    Add variables doc

---
+++ variables.js
@@ -0,0 +1,9 @@
+function kingdom() {
+    if (true) {
+    let prince = "Lancelot";
+      console.log(prince); // Output: Lancelot
+    }
+    console.log(prince); // Output: ❌ Error
+    // Prince has no power outside his region
+}
+kingdom();
